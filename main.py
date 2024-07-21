8b2dcf0cc5ec4cff5511ca0f8867805062ef646c
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-21 18:53:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
+import os
 import yt_dlp
 
-def download_youtube_shorts(channel_url, username):
+def download_channel_videos(channel_url, output_path, max_duration=None):
     ydl_opts = {
         'format': 'best',
-        'outtmpl': f'C:\\Users\\noway\\OneDrive\\사진\\test\\{username}\\%(upload_date)s_%(title)s.%(ext)s',
-        'max_duration': 60,
+        'outtmpl': os.path.join(output_path, '%(upload_date)s_%(title)s.%(ext)s'),
         'ignoreerrors': True,
         'nocheckcertificate': True,
         'quiet': False,
         'verbose': True,
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        # Remove the 'cookiefile' line if you don't have a cookies.txt file
-        # 'cookiefile': 'path/to/your/cookies.txt',
         'ratelimit': 500 * 1024,  # Setting rate limit as an integer in bytes
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
@@ -22,11 +20,41 @@ def download_youtube_shorts(channel_url, username):
         'compat_opts': set()
     }
 
+    if max_duration:
+        ydl_opts['max_duration'] = max_duration
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([channel_url])
 
+def main():
+    print("Paste YouTube channel link:")
+    ytlink = input().strip()
+
+    parts = ytlink.split("@")
+    if len(parts) > 1:
+        username = parts[1]
+    else:
+        print("Error: Invalid YouTube channel link format.")
+        return
+
+    channel_url = f'https://www.youtube.com/@{username}'
+
+    main_path = "C:\\Users\\noway\\OneDrive\\사진\\test"
+    output_path = os.path.join(main_path, username)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    shorts_path = os.path.join(output_path, "shorts")
+    playlists_path = os.path.join(output_path, "playlists")
+    video_path = os.path.join(output_path, "video")
+
+    for path in [shorts_path, playlists_path, video_path]:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    download_channel_videos(channel_url + "/shorts", shorts_path, max_duration=60)
+    download_channel_videos(channel_url + "/playlists", playlists_path)
+    download_channel_videos(channel_url + "/videos", video_path)
+
 if __name__ == "__main__":
-    channel_url = input("Paste YouTube channel link: ")
-    username = input("Enter the username: ")
-    download_youtube_shorts(channel_url, username)
- 
+    main()
