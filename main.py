64efefe4b52c0340f25819693f056f09d3b9ae08
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-21 18:57:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
-import yt_dlp
+import yt_dlp ##https://pypi.org/project/yt-dlp/
 
 def download_channel_videos(channel_url, output_path, max_duration=None):
     ydl_opts = {
-        'format': 'best',
+        'format': 'best', ##idk
         'outtmpl': os.path.join(output_path, '%(upload_date)s_%(title)s.%(ext)s'),
         'ignoreerrors': True,
         'nocheckcertificate': True,
         'quiet': False,
         'verbose': True,
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'ratelimit': 500 * 1024,  # Setting rate limit as an integer in bytes
+        'ratelimit': 500 * 1024,  
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -39,7 +39,7 @@ def main():
 
     channel_url = f'https://www.youtube.com/@{username}'
 
-    main_path = "C:\\Users\\noway\\OneDrive\\사진\\test"
+    main_path = "C:\\dir" ##C:\\Users\\noway\\OneDrive\\사진\\test
     output_path = os.path.join(main_path, username)
 
     if not os.path.exists(output_path):
@@ -52,6 +52,7 @@ def main():
     for path in [shorts_path, playlists_path, video_path]:
         if not os.path.exists(path):
             os.makedirs(path)
+
     download_channel_videos(channel_url + "/shorts", shorts_path, max_duration=60)
     download_channel_videos(channel_url + "/playlists", playlists_path)
     download_channel_videos(channel_url + "/videos", video_path)
