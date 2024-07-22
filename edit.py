865b9a3a9678e44aca30f7962d7cd76f60499dec
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-23 00:11:52 +0900

    Update edit.py

--- edit.py
+++ edit.py
@@ -5,8 +5,8 @@
 # save download date in json
 # save channel name too.
 
-#read if there is json
-#download only videos that are over that saved date
+# read if there is json
+# download only videos that are over that saved date
 
-#ifelse there isnt json just passby.
+# if there isnt json just passby.
 
