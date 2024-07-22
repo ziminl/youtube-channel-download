84be1cd5960b1e79e36a93c3cf02be06135bf980
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-22 23:29:38 +0900

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,1 +1,12 @@
 ##add record
+
+
+##logic 
+# save download date in json
+# save channel name too.
+
+#read if there is json
+#download only videos that are over that saved date
+
+#ifelse there isnt json just passby.
+
